3e098de404f5804d821ca4868eee9ebddf324688
Mamdouh Douidar <mamdouidar@gmail.com>
2023-03-03 13:29:08 +0200

    String & Int Tests With xUnit and Fluent Assertions

--- NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
+++ NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FluentAssertions;
+using NetworkUtility.Ping;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,36 @@ namespace NetworkUtility.Tests.PingTests
 {
     public class NetworkServiceTests
     {
+        [Fact]
+        public void NetworkService_SendPing_ReturnString()
+        {
+            // Arrange - Get Data Needed for the Test (Variables, Classes, Mocks)
+            var pingService = new NetworkService();
+
+            // Act - Execute the Unit
+            var result = pingService.SendPing();
+
+            // Assert
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be("Success: Ping Sent!");
+            result.Should().Contain("Success", Exactly.Once());
+        }
+
+        [Theory]
+        [InlineData(1, 1, 2)]
+        [InlineData(5, 8, 13)]
+        public void NetworkService_PingTimeout_ReturnInt(int a, int b, int expected)
+        {
+            // Arrange
+            var pingService = new NetworkService();
+
+            // Act
+            var result = pingService.PingTimeout(a, b);
+
+            //Assert
+            result.Should().Be(expected);
+            result.Should().BeGreaterThanOrEqualTo(2);
+            result.Should().NotBeInRange(-100, 0);
+        }
     }
 }
--- NetworkUtility/Ping/NetworkService.cs
+++ NetworkUtility/Ping/NetworkService.cs
@@ -8,5 +8,14 @@ namespace NetworkUtility.Ping
 {
     public class NetworkService
     {
+        public string SendPing()
+        {
+            return "Success: Ping Sent!";
+        }
+
+        public int PingTimeout(int a, int b)
+        {
+            return a + b;
+        }
     }
 }
